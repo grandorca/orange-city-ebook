c96a8fa6c77fd815b74fc7b174d72f6f4907560f
moohka <moohka@hotmail.com>
2022-08-23 20:41:40 -0500

    Header finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,35 +2,44 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  //today is...
   const [today, setToday] = useState();
 
   useEffect(() => {
+    //display today's date
     let options = {
       weekday: "long",
       year: "numeric",
       month: "long",
       day: "numeric",
     };
+
     let todayFormatted = new Date().toLocaleDateString("en-US", options);
+
     setToday(todayFormatted);
   }, []);
 
   return (
     <header>
-      <h1 className="header-title">Orange City Commnunity E-Bookstore</h1>
+      <h1 className="header-title">
+        <p>Orange City</p>
+        <p>Bookstore</p>
+      </h1>
 
-      <div className="today-date">
-        <span>{today}</span>
+      <div className="header-right">
+        <div className="today-date">
+          <span>{today}</span>
+        </div>
+
+        <nav>
+          <Link to="/" className="nav-anchor">
+            Search
+          </Link>
+          &nbsp;|&nbsp;
+          <Link to="/contact" className="nav-anchor">
+            Contact
+          </Link>
+        </nav>
       </div>
-
-      <Link to="/" className="nav-anchor">
-        Search
-      </Link>
-
-      <Link to="/contact" className="nav-anchor">
-        Contact
-      </Link>
     </header>
   );
 };
