a0fe56ebf6e0c52516eb45dfdd4a1cd8242c5b56
moohka <moohka@hotmail.com>
2022-08-26 01:59:26 -0500

    SF fix

--- netlify/functions/fetchBooks.js
+++ netlify/functions/fetchBooks.js
@@ -5,7 +5,7 @@ exports.handler = async function (event) {
     const query = event.queryStringParameters.query;
 
     const { data } = await axios.get(
-      `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`
+      `https://www.googleapis.com/books/v1/volumes?q=${query}`
     );
 
     return {
