e7760729b6fe39fca9d75b0ad61a7bd8e4ff724a
moohka <moohka@hotmail.com>
2022-08-26 01:35:43 -0500

    SF fix

--- src/components/elements/Display.jsx
+++ src/components/elements/Display.jsx
@@ -9,77 +9,85 @@ const Display = (props) => {
   const [bookModal, setBookModal] = useState({});
   const [modalOpen, setModalOpen] = useState(false);
 
-  const openModal = ({ bookInfo }) => {
-    setBookModal({ title: bookInfo.title, description: bookInfo.description });
-    console.log(bookInfo.description);
+  const openModal = ({ bookVolInfo }) => {
+    setBookModal({
+      title: bookVolInfo.title,
+      description: bookVolInfo.description,
+    });
+    console.log(bookVolInfo.description);
     setModalOpen(true);
   };
 
+  console.log(books);
   return (
     <div className="display">
       {/* each book card */}
-      {books.map((book) => {
-        // let indexOfThisBook = books.indexOf(book) + 1;
-        let bookInfo = book.volumeInfo;
-        let bookCover =
-          bookInfo.imageLinks && bookInfo.imageLinks.smallThumbnail;
+      {books
+        ? books.map((book) => {
+            // let indexOfThisBook = books.indexOf(book) + 1;
+            let bookVolInfo = book.volumeInfo;
+            let bookCover =
+              bookVolInfo.imageLinks && bookVolInfo.imageLinks.smallThumbnail;
 
-        return (
-          <div className="book-card" key={book.id}>
-            <img
-              className="book-item"
-              id="book-cover"
-              alt="book cover"
-              src={bookCover}
-              book-id={book.id}
-              onClick={() => {
-                openModal({ bookInfo });
-              }}
-            ></img>
-
-            <div className="book-detail">
-              <h2 className="book-item" id="book-title">
-                <span
+            return (
+              <div className="book-card" key={book.id}>
+                <img
+                  className="book-item"
+                  id="book-cover"
+                  alt="book cover"
+                  src={bookCover}
+                  book-id={book.id}
                   onClick={() => {
-                    openModal({ bookInfo });
+                    openModal({ bookVolInfo });
                   }}
-                >
-                  {bookInfo.title}
-                </span>
-              </h2>
-              <p className="book-item" id="book-authors">
-                {bookInfo.authors.length > 1 ? (
-                  <span>
-                    <span className="detail-sub-title">Authors:&nbsp;</span>
-                    {bookInfo.authors.map((author) => {
-                      return (
-                        <span key={author}>
-                          {author}&nbsp;&nbsp;|&nbsp;&nbsp;
-                        </span>
-                      );
-                    })}
-                  </span>
-                ) : (
-                  <span>
-                    <span className="detail-sub-title">Author:&nbsp;</span>
-                    {bookInfo.authors.map((author) => {
-                      return <span key={author}>{author}</span>;
-                    })}
-                  </span>
-                )}
-              </p>
-              <p className="book-item" id="book-pulisher">
-                <span className="detail-sub-title">Publisher:&nbsp;</span>
-                {bookInfo.publisher}
-              </p>
-              <p className="book-item" id="book-publishedDate">
-                <span className="detail-sub-title">Published Date:&nbsp;</span>
-                {bookInfo.publishedDate}
-              </p>
-            </div>
-          </div>
-        );
-      })}
+                ></img>
+
+                <div className="book-detail">
+                  <h2 className="book-item" id="book-title">
+                    <span
+                      onClick={() => {
+                        openModal({ bookVolInfo });
+                      }}
+                    >
+                      {bookVolInfo.title}
+                    </span>
+                  </h2>
+                  <p className="book-item" id="book-authors">
+                    {bookVolInfo.authors && bookVolInfo.authors.length > 1 ? (
+                      <span>
+                        <span className="detail-sub-title">Authors:&nbsp;</span>
+                        {bookVolInfo.authors.map((author) => {
+                          return (
+                            <span key={author}>
+                              {author}&nbsp;&nbsp;|&nbsp;&nbsp;
+                            </span>
+                          );
+                        })}
+                      </span>
+                    ) : (
+                      <span>
+                        <span className="detail-sub-title">Author:&nbsp;</span>
+                        {bookVolInfo.authors.map((author) => {
+                          return <span key={author}>{author}</span>;
+                        })}
+                      </span>
+                    )}
+                  </p>
+                  <p className="book-item" id="book-pulisher">
+                    <span className="detail-sub-title">Publisher:&nbsp;</span>
+                    {bookVolInfo.publisher}
+                  </p>
+                  <p className="book-item" id="book-publishedDate">
+                    <span className="detail-sub-title">
+                      Published Date:&nbsp;
+                    </span>
+                    {bookVolInfo.publishedDate}
+                  </p>
+                </div>
+              </div>
+            );
+          })
+        : null}
 
       {/* book description modal with full book title */}
       <Modal
